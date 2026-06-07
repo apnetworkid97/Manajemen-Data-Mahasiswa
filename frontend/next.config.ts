0a39693849799e8d3dd38e5c3596e60e6e043485
Aditya <adityagus6969@gmail.com>
2026-06-07 22:25:20 +0700

    update pack

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,12 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.100.*"],
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
+
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
   },
 };
 
