96720f57a8683982e037f3bc06227f2616e35b38
aditya agus <adityaagus161@gmail.com>
2026-06-12 00:14:56 +0700

    Update Server-Side Datatable

--- Backup/frontend/src/app/(full-width-pages)/layout.tsx
+++
@@ -1,8 +0,0 @@
-// Fungsi ini digunakan untuk menangani proses sesuai nama dan konteks pemanggilannya.
-export default function FullWidthPageLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div>{children}</div>;
-}
